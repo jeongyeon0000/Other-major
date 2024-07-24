764ec312f54dc35697f48578d23e0ea0582b9f3f
Jeongyeon_Song <songj239587@gmail.com>
2024-07-24 11:17:07 +0900

    리포지토리 이동

--- Python/변수와 자료형.py
+++
@@ -1,8 +0,0 @@
-a = 1
-aa = 1.0
-print(a == aa) # 값만 본다.
-print(a is aa) # 자료형을 본다.
-aaa = input("1을 입력하시오.")
-print(a,"는",aaa,"와 같은가?", a == aaa) # input으로 입력되는 값은 문자이다.
-aaa = int(aaa) # int형으로 바꾼다.
-print(a,"는",aaa,"와 같은가?", a == aaa)
